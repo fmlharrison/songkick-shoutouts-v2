1f05ea240b9c76bd158f5d54769ffc5573cb753c
Felix Harrison <felix.harrison@songkick.com>
2019-10-02 14:18:57 +0100

    Use hooks for sign up/in forms

--- src/components/Authentication/SignIn/SignInForm.js
+++ src/components/Authentication/SignIn/SignInForm.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Link, withRouter } from "react-router-dom";
 import { compose } from "recompose";
 
@@ -11,77 +11,68 @@ const INITIAL_STATE = {
   error: null
 };
 
-class SignInForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { ...INITIAL_STATE };
-  }
+const SignInForm = props => {
+  const [email, setEmail] = useState(INITIAL_STATE.email);
+  const [password, setPassword] = useState(INITIAL_STATE.password);
+  const [error, setError] = useState(INITIAL_STATE.error);
 
-  onChange = event => {
-    const targetEvent = event.target;
-    this.setState({ [targetEvent.name]: targetEvent.value });
-  };
-
-  onSubmit = event => {
+  const onSubmit = event => {
     event.preventDefault();
-    const { email, password } = this.state;
 
-    this.props.firebase
+    props.firebase
       .doSignInWithEmailAndPassword(email, password)
       .then(authUser => {
-        this.props.firebase.user(authUser.user.uid).once("value", snapshot => {
+        props.firebase.user(authUser.user.uid).once("value", snapshot => {
           const user = snapshot.val();
           console.log(user);
         });
       })
       .then(user => {
-        this.setState({ ...INITIAL_STATE });
-        this.props.history.push(routes.LANDING);
+        setEmail(INITIAL_STATE.email);
+        setPassword(INITIAL_STATE.password);
+        setError(INITIAL_STATE.error);
+        props.history.push(routes.LANDING);
       })
       .catch(error => {
-        this.setState({ error });
+        setError(error);
       });
   };
 
-  render() {
-    const { email, password, error } = this.state;
+  const isInvalid = password === "" || email === "";
 
-    const isInvalid = password === "" || email === "";
+  return (
+    <div>
+      <form onSubmit={onSubmit} className="sign-in-form">
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={event => setEmail(event.target.value)}
+          placeholder="Enter a email"
+          className="form-input"
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={event => setPassword(event.target.value)}
+          placeholder="Enter a password"
+          className="form-input"
+          required
+        />
+        <button type="submit" disabled={isInvalid} className="sign-in-button">
+          Sign In
+        </button>
 
-    return (
-      <div>
-        <form onSubmit={this.onSubmit} className="sign-in-form">
-          <input
-            type="email"
-            name="email"
-            value={email}
-            onChange={this.onChange}
-            placeholder="Enter a email"
-            className="form-input"
-            required
-          />
-          <input
-            type="password"
-            name="password"
-            value={password}
-            onChange={this.onChange}
-            placeholder="Enter a password"
-            className="form-input"
-            required
-          />
-          <button type="submit" disabled={isInvalid} className="sign-in-button">
-            Sign In
-          </button>
-
-          {error && <p className="error-message">{error.message}</p>}
-        </form>
-        <div className="sign-up-link">
-          Don't have an account? <Link to={routes.SIGN_UP}>Sign Up</Link>
-        </div>
+        {error && <p className="error-message">{error.message}</p>}
+      </form>
+      <div className="sign-up-link">
+        Don't have an account? <Link to={routes.SIGN_UP}>Sign Up</Link>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default compose(
   withRouter,
--- src/components/Authentication/Signup/SignUpForm.js
+++ src/components/Authentication/Signup/SignUpForm.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Link, withRouter } from "react-router-dom";
 import { compose } from "recompose";
 
@@ -14,18 +14,17 @@ const INITIAL_STATE = {
   error: null
 };
 
-class SignUpForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { ...INITIAL_STATE };
-  }
+const SignUpForm = props => {
+  const [displayName, setDisplayName] = useState(INITIAL_STATE.displayName);
+  const [username, setUsername] = useState(INITIAL_STATE.username);
+  const [email, setEmail] = useState(INITIAL_STATE.email);
+  const [password, setPassword] = useState(INITIAL_STATE.password);
+  const [repeatedPassword, setRepeatedPassword] = useState(
+    INITIAL_STATE.repeatedPassword
+  );
+  const [error, setError] = useState(INITIAL_STATE.error);
 
-  onChange = event => {
-    const targetEvent = event.target;
-    this.setState({ [targetEvent.name]: targetEvent.value });
-  };
-
-  onSubmit = event => {
+  const onSubmit = event => {
     event.preventDefault();
     const { displayName, username, email, password } = this.state;
 
@@ -35,7 +34,7 @@ class SignUpForm extends Component {
       });
     }
 
-    this.props.firebase
+    props.firebase
       .doCreateUserWithEmailAndPassword(email, password)
       .then(authUser => {
         authUser.user.updateProfile({ displayName: displayName });
@@ -43,7 +42,7 @@ class SignUpForm extends Component {
       })
       .then(user => {
         const id = user.user.uid;
-        return this.props.firebase.user(id).set({
+        return props.firebase.user(id).set({
           id,
           displayName,
           username,
@@ -51,92 +50,86 @@ class SignUpForm extends Component {
         });
       })
       .then(() => {
-        this.setState({ ...INITIAL_STATE });
-        this.props.history.push(routes.LANDING);
+        setDisplayName(INITIAL_STATE.displayName);
+        setUsername(INITIAL_STATE.username);
+        setEmail(INITIAL_STATE.email);
+        setPassword(INITIAL_STATE.password);
+        setRepeatedPassword(INITIAL_STATE.repeatedPassword);
+        setError(INITIAL_STATE.error);
+        props.history.push(routes.LANDING);
       })
       .catch(error => {
-        this.setState({ error });
+        setError(error);
       });
   };
 
-  render() {
-    const {
-      displayName,
-      username,
-      email,
-      password,
-      repeatedPassword,
-      error
-    } = this.state;
+  const isInvalid =
+    password !== repeatedPassword ||
+    password === "" ||
+    displayName === "" ||
+    username === "" ||
+    email === "";
 
-    const isInvalid =
-      password !== repeatedPassword ||
-      password === "" ||
-      displayName === "" ||
-      username === "" ||
-      email === "";
+  return (
+    <div>
+      <form onSubmit={onSubmit} className="sign-up-form">
+        <input
+          type="text"
+          name="displayName"
+          value={displayName}
+          onChange={event => setDisplayName(event.target.value)}
+          placeholder="Enter a display name."
+          className="form-input"
+          required
+        />
+        <input
+          type="text"
+          name="username"
+          value={username}
+          onChange={event => setUsername(event.target.value)}
+          placeholder="Enter a username."
+          className="form-input"
+          required
+        />
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={event => setEmail(event.target.value)}
+          placeholder="Enter a email."
+          className="form-input"
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={event => setPassword(event.target.value)}
+          placeholder="Enter a password."
+          className="form-input"
+          required
+        />
+        <input
+          type="password"
+          name="repeatedPassword"
+          value={repeatedPassword}
+          onChange={event => setRepeatedPassword(event.target.value)}
+          placeholder="Re-enter your password."
+          className="form-input"
+          required
+        />
+        <button type="submit" disabled={isInvalid} className="sign-up-button">
+          Sign Up
+        </button>
 
-    return (
-      <div>
-        <form onSubmit={this.onSubmit} className="sign-up-form">
-          <input
-            type="text"
-            name="displayName"
-            value={displayName}
-            onChange={this.onChange}
-            placeholder="Enter a display name."
-            className="form-input"
-            required
-          />
-          <input
-            type="text"
-            name="username"
-            value={username}
-            onChange={this.onChange}
-            placeholder="Enter a username."
-            className="form-input"
-            required
-          />
-          <input
-            type="email"
-            name="email"
-            value={email}
-            onChange={this.onChange}
-            placeholder="Enter a email."
-            className="form-input"
-            required
-          />
-          <input
-            type="password"
-            name="password"
-            value={password}
-            onChange={this.onChange}
-            placeholder="Enter a password."
-            className="form-input"
-            required
-          />
-          <input
-            type="password"
-            name="repeatedPassword"
-            value={repeatedPassword}
-            onChange={this.onChange}
-            placeholder="Re-enter your password."
-            className="form-input"
-            required
-          />
-          <button type="submit" disabled={isInvalid} className="sign-up-button">
-            Sign Up
-          </button>
-
-          {error && <p className="error-message">{error.message}</p>}
-        </form>
-        <div className="sign-in-link">
-          Already have an account? <Link to={routes.SIGN_IN}>Sign In</Link>
-        </div>
+        {error && <p className="error-message">{error.message}</p>}
+      </form>
+      <div className="sign-in-link">
+        Already have an account? <Link to={routes.SIGN_IN}>Sign In</Link>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default compose(
   withRouter,
